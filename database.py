19a3875e4b67f15217660fa005406de078849403
sangadang <sangadang@kms-technology.com>
2026-02-24 18:35:13 +0700

    Dockerfile

--- database.py
+++ database.py
@@ -8,9 +8,7 @@ from sqlalchemy import Column, DateTime, create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 load_dotenv()
-credential = DefaultAzureCredential(
-    client_id='d6430b79-a02c-4d59-954f-d8b98956352f',
-)
+credential = DefaultAzureCredential()
 
 def get_connection():
     token = credential.get_token("https://database.windows.net/.default")
