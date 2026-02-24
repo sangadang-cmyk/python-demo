f8901e15f509a1198d81cf8fdee79fa315781f64
sangadang <sangadang@kms-technology.com>
2026-02-24 18:21:36 +0700

    Dockerfile

--- database.py
+++ database.py
@@ -1,39 +1,31 @@
 from datetime import datetime
 import os
 import struct
+import pyodbc
 from dotenv import load_dotenv
 from azure.identity import DefaultAzureCredential
-from sqlalchemy import URL, Column, DateTime, create_engine, event
+from sqlalchemy import Column, DateTime, create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 load_dotenv()
-
 credential = DefaultAzureCredential()
 
-connection_url = URL.create(
-    "mssql+pyodbc",
-    host=os.getenv("DB_HOST"),
-    port=1433,
-    database=os.getenv("DB_NAME"),
-    query={
-        "driver": "ODBC Driver 18 for SQL Server",
-        "Encrypt": "yes",
-        "TrustServerCertificate": "yes",
-    },
-)
-
-engine = create_engine(connection_url, echo=True)
-
-@event.listens_for(engine, "do_connect")
-def provide_token(dialect, conn_rec, cargs, cparams):
+def get_connection():
     token = credential.get_token("https://database.windows.net/.default")
     token_bytes = token.token.encode("utf-16-le")
     token_struct = struct.pack(f"<I{len(token_bytes)}s", len(token_bytes), token_bytes)
-    cparams["attrs_before"] = {1256: token_struct}
+    conn_str = (
+        f"Driver={{ODBC Driver 18 for SQL Server}};"
+        f"Server={os.getenv('DB_HOST')},1433;"
+        f"Database={os.getenv('DB_NAME')};"
+        f"Encrypt=yes;"
+        f"TrustServerCertificate=yes;"
+    )
+    return pyodbc.connect(conn_str, attrs_before={1256: token_struct})
 
+engine = create_engine("mssql+pyodbc://", creator=get_connection, echo=True)
 SessionLocal = sessionmaker(bind=engine)
 
 class Base(DeclarativeBase):
     createdAt = Column(DateTime, default=datetime.now)
     updatedAt = Column(DateTime, onupdate=datetime.now)
-    pass
